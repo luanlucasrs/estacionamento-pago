8bccee1f8f385113c5ae75e9663d8c9c9cbb2bbc
renanalmeids <58474539+renanalmeids@users.noreply.github.com>
2021-05-03 23:37:04 -0300

    Create DadosVeiculosIncompletosException.java

---
+++ DadosVeiculosIncompletosException.java
@@ -0,0 +1,12 @@
+package estacionamento.exceptions;
+
+
+public class DadosVeiculosIncompletosException {
+ import unb.estacionamentp.exceptions.Exception;
+ 
+ public classDadosVeiculosincompletosException extends Exception{
+	 private static final  long serialVersionUID = - -2843490224517817163L;
+ }
+}
+
+
