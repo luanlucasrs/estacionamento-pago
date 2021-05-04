a876911c8383fbec4b808dd2eeabd9aae574c66a
martins-mariana <marianasmartinss@gmail.com>
2021-05-03 23:47:20 -0300

    Update DadosPessoaisIncompletoException.java

--- DadosPessoaisIncompletoException.java
+++ DadosPessoaisIncompletoException.java
@@ -1,6 +1,6 @@
 package ProjetoFinalOO.exceptions;
 
-public class DadosPessoaisImcompletosException extends Exception {
+public class DadosPessoaisIncompletosException extends Exception {
 	
 	private static final long serialVersionUID = 1299269911307701227L;
 
