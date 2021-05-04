4034facc027ac267712fdfda2db2b9a8e6f0d3cd
luanlucasrs <luanlucasbsb@gmail.com>
2021-05-03 23:48:14 -0300

    Create Cobranca.java

---
+++ Cobranca.java
@@ -0,0 +1,46 @@
+package ProjetoFinalOO;
+
+public class Cobranca {
+
+	private double porMinuto;
+	private double porFracao;
+	private double porHora;
+	private double porDiaria;
+	private double porPerNoite;
+	private double porMes;
+	
+	public Cobranca (double porMinuto, double porFracao, double porHora, double porDiaria, double porPerNoite, double porMes){
+		this.porMinuto = porMinuto;
+		this.porFracao = porFracao;
+		this.porHora = porHora;
+		this.porDiaria = porDiaria;
+		this.porPerNoite = porPerNoite;
+		this.porMes = porMes;
+	}
+	
+	public double getPorMinuto() {
+		return porMinuto;
+	}
+
+	public double getPorFracao() {
+		return porFracao;
+	}
+
+	public double getPorHora() {
+		return porHora;
+	}
+
+	public double getPorDiaria() {
+		return porDiaria;
+	}
+
+	public double getPorPerNoite() {
+		return porPerNoite;
+	}
+
+	public double getPorMes() {
+		return porMes;
+	}
+	
+	
+}
