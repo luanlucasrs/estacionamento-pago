2c9400f06700c4fc3bc08c64f6ea5ff94ca608fc
mylenar <mylenarao@gmail.com>
2021-05-20 15:04:26 -0300

    Update Veiculo.java
    Classe atualizada. Retirado métodos de busca e passado pra main.

--- src/projetoFinalOO/Veiculo.java
+++ src/projetoFinalOO/Veiculo.java
@@ -2,22 +2,27 @@ package projetoFinalOO;
 
 public class Veiculo {
 
-	private String placa;
+	private String placa; //atributos privados para seguranca (não pode alterar direto)
 	private String marca;
 	private String modelo;
 	private boolean mensalista;
+
 	
-	public Veiculo(String placa, String marca, String modelo, boolean mensalista) {
+	
+	// Método Construtor
+	
+	public Veiculo(String placa, String marca, String modelo, boolean mensalista) { 
 		this.placa = placa;
 		this.marca = marca;
 		this.modelo = modelo;
 		this.mensalista = mensalista;
+		
 	}
 	
-	public Veiculo() {
-	
-	}
+		
 
+	// getters and setters
+	
 	public String getPlaca() {
 		return placa;
 	}
@@ -42,11 +47,59 @@ public class Veiculo {
 		this.modelo = modelo;
 	}
 
-	public boolean isMensalista() {
-		return mensalista;
-	}
-
+//	public boolean  getMensalista() {
+//		return mensalista;
+//	}
 	public void setMensalista(boolean mensalista) {
 		this.mensalista = mensalista;
 	}
+
+
+
+	public boolean getMensalista() {
+		// TODO Auto-generated method stub
+		return mensalista;
+	}
+	
+	
+	// outros metodos
+	
+	
+	
+	// adicionando todos dos carros a uma lista
+//	public void addVeiculo (Veiculo CadastroVeiculo) {
+		
+//		if (CadastroVeiculo.mensalista == true) {
+			
+	//		veiculosMensalistas.add(CadastroVeiculo); // Adicionando a lista especifica só de mensalistas
+	//	}
+		
+	//	veiculos.add(CadastroVeiculo); // Adicionando todos a uma outra lista (mensalistas ou não)
+			
+	//}
+	
+	
+	// buscar uma placa dentro das listas
+	
+	//Caso a busca seja feita na lista APENAS de mensalistas
+	
+//	
+	
+	
+	//Metodo de busca geral
+	
+//	public Veiculo BuscaPlaca(String placa) {
+
+  //      for (Veiculo veiculo : veiculos) {
+
+    //        if (veiculo.getPlaca().equals(placa)) {
+
+      //          return veiculo;
+        //    }
+
+        //}
+
+       // return null;
+    //}
+	
 }
